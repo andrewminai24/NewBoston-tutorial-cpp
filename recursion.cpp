5eae4da3039d87df9c155e1c9b6c7f4365ac136c
1 <a1@MacBook-Pro-1.attlocal.net>
2019-06-27 20:39:11 -0700

    recursion

---
+++ recursion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int FactorialFinder(int x){
+    if(x == 1){
+        return 1;
+    } else {
+         return x*FactorialFinder(x-1);
+    }
+}
+
+
+int main(){
+    cout << FactorialFinder(5) << endl;
+}
