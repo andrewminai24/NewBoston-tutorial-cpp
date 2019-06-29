70e55e2b36f9ce90fe51075eb2ae198a5b9cd9e6
Andrew Minai <user.email>
2019-06-28 17:10:32 -0700

    characters

---
+++ class/characters.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Enemy {
+    public:
+        virtual void attack(){
+            cout << "I am the enemy class" << endl;
+
+        };
+
+};
+
+class Ninja: public Enemy {
+    public: 
+        void attack()
+        {
+            cout << "ninja attack!" << endl;
+        }
+};
+class Monster: public Enemy {
+    public:
+        void attack(){
+            cout << "monster attack" << endl;
+        }
+};
+
+int main(void)
+{
+    Ninja n;
+    Monster m;
+    Enemy *enemy1 = &n;
+    Enemy *enemy2 = &m;
+    enemy1->attack();
+    enemy1->attack();
+
+}
